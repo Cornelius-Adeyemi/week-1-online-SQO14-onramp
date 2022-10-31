d1be844fe9cba97876c579a2e0fca155dca7d591
Adebisi Adeyem <aadebisi119@gmail.com>
2022-10-31 14:58:49 -0400

    newUpdate

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,7 @@
 function fizzBuzz(input){
 
     if(input % 3 == 0 && input % 5==0 ){
-        return "fizzbuzz";
+        return "fizzBuzz";
       }else if( input % 3 == 0){
        return "fizz"
       }else if(input % 5 == 0){
